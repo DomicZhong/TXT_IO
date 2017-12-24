7af0f4ad4d6c60a2bc59acc2807eef86ded31bd0
DomicZhong <zxm1999@foxmail.com>
2017-12-24 12:02:08 +0800

    添加项目文件。

---
+++ Txt_File_IO/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Txt_File_IO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path1 = @"C:\Users\jhk\Desktop\test.txt";   //文件路径
+            string path2 = @"C:\Users\jhk\Desktop\test1.txt";
+            string[] strings;
+
+            strings = TxtFileIO.TxtFileRead(path1);
+
+            foreach (string str in strings)     //输出path1文件内容
+            {
+                Console.WriteLine(str);
+            }
+
+            TxtFileIO.TxtFileWrite(path2, strings, true); //在path2文件写入strings里的内容
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Txt_File_IO/TxtFileIO.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace TxtFileIO
+{
+    class TxtFileIO
+    {
+        public static string[] TxtFileRead(string file_name)     //读txt函数
+        {
+            string[] strs= new string[100];
+            if (File.Exists(file_name))     //判断文件是否存在
+            {
+                using (StreamReader sr = new StreamReader(file_name))
+                {
+                    string line;
+                    int i = 0;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        strs[i++] = line;
+                    }
+                }
+            }
+            return strs;
+        }
+
+        public static bool TxtFileWrite(string file_name, string[] strs,bool writeMode)   //x写txt函数
+        {
+            try
+            {
+                if (!File.Exists(file_name))  //判断文件是否存在
+                {
+                    File.Create(file_name);   //不存在的话，则创建文件
+                }
+            }
+            catch(Exception e)     //捕获异常
+            {
+                Console.WriteLine(e.Message);
+                return false;   //返回false
+            }
+
+            using (StreamWriter sr = new StreamWriter(file_name,writeMode))   //追加模式: writeMode=true
+            {
+                int i = 0;
+                while ((strs[i]) != null)    
+                {
+                    sr.WriteLine(strs[i++]);
+                }
+                return true;
+            }
+        }
+    }
+}
